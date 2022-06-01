91b20b5d412cd0328024ec77c039ec4e62e4264c
Ivan Volkov <volkivanv@mail.ru>
2022-06-01 20:03:01 +0300

    second commit

--- calculatormainwindow.h
+++ calculatormainwindow.h
@@ -10,6 +10,8 @@
 #include <string>
 
 
+
+
 class CalculatorMainWindow : public QMainWindow{
 Q_OBJECT
 public:
@@ -24,37 +26,92 @@ public:
 public slots:
     void slotSum(){
 
-        double a = std::stod(textEdit->toMarkdown().toStdString());
-        textEdit->setText(std::to_string(a).c_str());
-        double b = std::stod(textEdit_2->toMarkdown().toStdString());
-        textEdit_2->setText(std::to_string(b).c_str());
-        textEdit_3->setText(std::to_string(a+b).c_str());
-    };
-    void slotSubtract(){
-        double a = std::stod(textEdit->toMarkdown().toStdString());
-        textEdit->setText(std::to_string(a).c_str());
-        double b = std::stod(textEdit_2->toMarkdown().toStdString());
-        textEdit_2->setText(std::to_string(b).c_str());
-        textEdit_3->setText(std::to_string(a - b).c_str());
-    };
-    void slotDivide(){
-        double a = std::stod(textEdit->toMarkdown().toStdString());
-        textEdit->setText(std::to_string(a).c_str());
-        double b = std::stod(textEdit_2->toMarkdown().toStdString());
-        textEdit_2->setText(std::to_string(b).c_str());
-        if(b!=0) {
-            textEdit_3->setText(std::to_string(a / b).c_str());
-        } else {
-            textEdit_3->setText("ERROR!");
+        std::string sA = textEdit-> toMarkdown().toStdString();
+        std::string sB = textEdit_2-> toMarkdown().toStdString();
+        if(isNumber(sA) && isNumber(sB)) {
+            double a = std::stod(sA);
+            double b = std::stod(sB);
+            textEdit_3 -> setText(std::to_string(a + b).c_str());
+        }else {
+            textEdit_3->setText("ERROR! Incorrect input!");
         }
     };
-    void slotMultiplicate(){
-        double a = std::stod(textEdit->toMarkdown().toStdString());
-        textEdit->setText(std::to_string(a).c_str());
-        double b = std::stod(textEdit_2->toMarkdown().toStdString());
-        textEdit_2->setText(std::to_string(b).c_str());
-        textEdit_3->setText(std::to_string(a*b).c_str());
+    void slotSubtract(){
+        std::string sA = textEdit-> toMarkdown().toStdString();
+        std::string sB = textEdit_2-> toMarkdown().toStdString();
+        if(isNumber(sA) && isNumber(sB)) {
+            double a = std::stod(sA);
+            double b = std::stod(sB);
+            textEdit_3 -> setText(std::to_string(a - b).c_str());
+        }else {
+            textEdit_3->setText("ERROR! Incorrect input!");
+        }
     };
+    void slotDivide(){
+        double a;
+        double b;
+        std::string sA = textEdit-> toMarkdown().toStdString();
+        std::string sB = textEdit_2-> toMarkdown().toStdString();
+        if(isNumber(sA) && isNumber(sB)) {
+            a = std::stod(sA);
+            b = std::stod(sB);
+            if(b!=0) {
+                textEdit_3->setText(std::to_string(a / b).c_str());
+            } else {
+                textEdit_3->setText("ERROR! Division by zero!");
+            }
+        }else {
+            textEdit_3->setText("ERROR! Incorrect input!");
+        }
+
+    };
+    void slotMultiplicate(){
+        std::string sA = textEdit-> toMarkdown().toStdString();
+        std::string sB = textEdit_2-> toMarkdown().toStdString();
+        if(isNumber(sA) && isNumber(sB)) {
+            double a = std::stod(sA);
+            double b = std::stod(sB);
+            textEdit_3 -> setText(std::to_string(a * b).c_str());
+        }else {
+            textEdit_3->setText("ERROR! Incorrect input!");
+        }
+    };
+private:
+    bool isNumber(std::string &s) {
+        int pointCount = 0;
+/*
+        for (int i = s.length()-1; i >=0; i--) {
+            if (((s[i] < '0') || (s[i] > '9'))&&(i == s.length() - 1)) {
+                s.pop_back();
+            }
+        }
+*/
+        for (int i = s.length()-1; i >=0; i--) {
+            if (((s[i] == '\n')||(s[i] == '.')||(s[i] == ','))&&(i == s.length() - 1)) {
+                s.pop_back();
+            }
+        }
+
+
+
+
+        for (int i = s.length()-1; i >=0; i--) {
+            if(s[i] == ',') s[i] ='.';
+
+            if ((s[i] == '.') || (s[i] == '-')){
+
+               if(s[i] == '.'){
+                    pointCount++;
+                    if(pointCount > 1) return false;
+               }
+               if((s[i] =='-')&&(i != 0)) {
+                    return false;
+               }
+
+            } else if((s[i] < '0') || (s[i] > '9')) return false;
+        }
+        return true;
+    }
 
 
 
